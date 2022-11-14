460507c5d58d006c87ec282ee57226024c545da7
El Issmail Al Assaf <selissma@t-systems.es>
2022-11-14 16:43:54 +0100

    belong to Supermarket

---
+++ Products.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.ejercicos1;
+
+/**
+ *
+ * @author selissma
+ */
+public class Products {
+    private String name;
+    private double price;
+
+    public Products(String name, double price ){
+        this.name=name;
+        this.price=price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return "Product name: " + this.name + " with price " + this.price;
+    }
+}
