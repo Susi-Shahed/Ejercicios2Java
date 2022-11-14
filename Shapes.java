890e74b36e8e7a9da7e1a6d7fb19f70fdd44f254
El Issmail Al Assaf <selissma@t-systems.es>
2022-11-14 16:45:52 +0100

    Shapes

---
+++ Shapes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.ejercicos1;
+
+/**
+ *
+ * @author selissma
+ */
+public abstract class Shapes {
+    private double area;
+    private double perimetro;
+    
+    public abstract double calculateArea();
+    public abstract double calculatePerimetro();
+
+}
